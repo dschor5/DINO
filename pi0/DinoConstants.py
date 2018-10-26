32a5bbe1ab9af2258d6b586a85e034b914261b31
Dario Schor <dschor5@gmail.com>
2018-10-26 14:37:58 -0500

    Cleaned up repeated constants. Many have been moved to individual classes where they are used the most.

--- pi0/DinoConstants.py
+++ pi0/DinoConstants.py
@@ -3,6 +3,25 @@
 # Constants
 ###############################################################################
 
+# Temperature thresholds in degree Celsius to match sensors
+# From: "HAB & DINOLAB Software Command Requirements.xlsx"
+TURN_ON_HEATER  = 16.9 # Celsius. Equivalent to 62.5F.
+TURN_OFF_HEATER = 21.1 # Celsius. Equivalent to 70.0F.
+TURN_ON_COOLER  = 25.8 # Celsius. Equivalent to 78.5F.
+TURN_OFF_COOLER = 18.3 # Celsius. Equivalent to 65.0F.
+
+# GPIO Pin numbers
+# From: Terry's email.
+SERVO_PIN  = 18
+HEATER_PIN = 16
+COOLER_PIN = 12
+
+# Camera recording duration in seconds
+CAMERA_REC_DURATION = 60
+
+# Servo agitation interval in seconds
+SERVO_AGITATION_INTERVAL = 4
+
 # Colors for printing output using ANSI escape sequences. 
 # From: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
 COLORS = {
@@ -16,67 +35,37 @@ COLORS = {
    'BOLD'       : '\033[1m'     # White/bold
    }
 
-
-# Possible flight states sent by New Shepard vehicle to the payload.
-# From: NR-BLUE-W0001 (RevA) Feather Frame Payload User's Guide (002).pdf
-FLIGHT_STATES = {
-   'NONE'         : '@',   # No flight state has been reached yet 
-                           #    (typically the time prior to liftoff).
-   'LIFTOFF'      : 'A',   # This state is triggered once sensed acceleration 
-                           #    first changes due to engine ignition.
-   'MECO'         : 'B',   # This state is triggered after the rocket's main engine 
-                           #    cuts out, and the flight enters its coast stage.
-   'SEPARATION'   : 'C',   # This state occurs after the rocket and capsule 
-                           #    separate, shortly before the microgravity 
-                           #    portion of the flight begins.
-   'COAST_START'  : 'D',   # This state indicates the beginning of the cleanest 
-                           #    microgravity operations onboard the capsule, 
-                           #    most experiments should begin logging data at this time.
-   'APOGEE'       : 'E',   # This state occurs when the vehicle has reached its 
-                           #    maximum altitude and begins to descend.
-   'COAST_END'    : 'F',   # This state indicates the end of microgravity operations 
-                           #    onboard the capsule, as we begin to experience 
-                           #    atmospheric accelerations. Many experiments will 
-                           #    cease logging data at this time.
-   'UNDER_CHUTES' : 'G',   # This state indicates that drogue parachutes have 
-                           #    deployed and the capsule is in its final descent.
-   'LANDING'      : 'H',   # This state occurs after capsule touchdown.
-   'SAFING'       : 'I',   # After touchdown, this state indicates that the 
-                           #    capsule is venting and safing all energetic systems.
-   'FINISHED'     : 'J'    # This state is only ever reached in simulation and 
-                           #    indicates the end of the logged flight data.
-   }
-
-
-
-
-
-# Telemetry keys recorded by the software in addition to the info from the vehicle.
-# From: "HAB & DINOLAB Software Command Requirements.xlsx"
-TLM_KEYS = [
-   'Timestamp',            # Calculated by the software independent of serial packet received.
-   'EnvLight',             # From Envirophat light sensor.
-   'EnvRed',               # From Envirophat light sensor.
-   'EnvGreen',             # From Envirophat light sensor.
-   'EnvBlue',              # From Envirophat light sensor.
-   'EnvTemperature',       # From Envirophat weather sensor.
-   'EnvPressure',          # From Envirophat weather sensor.
-   'EnvAccelerationX',     # From Envirophat motion sensor.
-   'EnvAccelerationY',     # From Envirophat motion sensor.
-   'EnvAccelerationZ'      # From Envirophat motion sensor.
-   ]
-
-# Temperature thresholds in degree Celsius to match sensors
-# From: "HAB & DINOLAB Software Command Requirements.xlsx"
-TURN_ON_HEATER  = 16.9 # Celsius. Equivalent to 62.5F.
-TURN_OFF_HEATER = 21.1 # Celsius. Equivalent to 70.0F.
-TURN_ON_COOLER  = 25.8 # Celsius. Equivalent to 78.5F.
-TURN_OFF_COOLER = 18.3 # Celsius. Equivalent to 65.0F.
-
-# GPIO Pin numbers
-# From: Terry's email.
-SERVO_PIN  = 18
-HEATER_PIN = 16
-COOLER_PIN = 12
+# Column names for parsing data
+I_FLIGHT_STATE    = 0   # Integer
+I_ALTITUDE        = 1   # Float
+I_VELOCITY_X      = 2   # Float      
+I_VELOCITY_Y      = 3   # Float
+I_VELOCITY_Z      = 4   # Float
+I_ACCELERATION    = 5   # Float
+I_ATTITUDE_X      = 6   # Float
+I_ATTITUDE_Y      = 7   # Float
+I_ATTITUDE_Z      = 8   # Float
+I_ANG_VEL_X       = 9   # Float
+I_ANG_VEL_Y       = 10  # Float
+I_ANG_VEL_Z       = 11  # Float
+I_WARNING_LIFTOFF = 12  # Integer
+I_WARNING_RCS     = 13  # Integer
+I_WARNING_ESCAPE  = 14  # Integer
+I_WARNING_CHUTE   = 15  # Integer
+I_WARNING_LANDING = 16  # Integer
+I_WARNING_FAULT   = 17  # Integer 
+I_LIGHT_RED       = 18  # Integer
+I_LIGHT_GREEN     = 19  # Integer
+I_LIGHT_BLUE      = 20  # Integer
+I_LIGHT_CLEAR     = 21  # Integer
+I_TEMPERATURE     = 22  # Float
+I_PRESSURE        = 23  # Float
+I_ACCEL_X         = 24  # Float
+I_ACCEL_Y         = 25  # Float
+I_ACCEL_Z         = 26  # Float
+I_MAG_X           = 27  # Float
+I_MAG_Y           = 28  # Float
+I_MAG_Z           = 29  # Float
+I_SIZE            = 30
 
 
