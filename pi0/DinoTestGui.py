4b536e4531c5cca3d9301cc478734ff87da84140
fbourbour <fredbourbour@comcast.net>
2018-12-31 18:06:13 +0000

    saving spectrometer offset after calibration

--- pi0/DinoTestGui.py
+++ pi0/DinoTestGui.py
@@ -5,11 +5,12 @@
 #
 ###########################################################################################
 
-from guizero import App, Text
+from guizero import App, Text, TextBox, warn, info
 from guizero import PushButton
 import os
 import csv
 import glob
+
 #import matplotlib.pyplot as plt
 
 
@@ -42,18 +43,33 @@ def test_heater():
 def test_serial():
   print("Testing the Serial Port")
   os.system("sudo python3 test.py DinoSerial")
+  
+def test_calibrate():
+  print("Calibrating The Spectrometer")
+  if(wavelength.get() == "" or wavelength.get() == "wavelength"):
+     warn("Error", "Enter a valid Wavelength")
+     return
+  print (wavelength.get())
+  os.system("sudo python3 test.py DinoSpectrometer")
+  spectrum_files = glob.glob ("*.csv")
+  last_spectrum_file = max(spectrum_files,key = os.path.getctime)
+  print (last_spectrum_file)
+  os.system("cp " + last_spectrum_file + " DinoCalibrate.csv")
+  os.system("sudo rm " + last_spectrum_file)
+  os.system("sudo python DinoCalibrate.py " + "DinoCalibrate.csv " +  wavelength.get())
 
 def test_simulation():
   print("Testing the Serial Port")
   os.system("sudo python3 test.py DinoSim")
   
- 
-button1 = PushButton(app, command=test_spectrometer, text="Spectrometer")
-button2 = PushButton(app, command=test_camera, text="Camera      ")
-button3 = PushButton(app, command=test_servo, text="Servo       ")
-button4 = PushButton(app, command=test_heater, text="Thermal Control")
-button5 = PushButton(app, command=test_serial, text="Serial         ")
-button6 = PushButton(app, command=test_simulation, text="Run Simulation")
+button1 = PushButton(app, command=test_calibrate, text="Calibrate")
+wavelength = TextBox(app,text = "wavelength")
+button2 = PushButton(app, command=test_spectrometer, text="Spectrometer")
+button3 = PushButton(app, command=test_camera, text="Camera      ")
+button4 = PushButton(app, command=test_servo, text="Servo       ")
+button5 = PushButton(app, command=test_heater, text="Thermal Control")
+button6 = PushButton(app, command=test_serial, text="Serial         ")
+button7 = PushButton(app, command=test_simulation, text="Run Simulation")
 
 
 button1.width = 12
@@ -62,6 +78,8 @@ button3.width = 12
 button4.width = 12
 button5.width = 12
 button6.width = 12
+button7.width = 12
+
 
 button1.padding(5,5)
 button2.padding(5,5)
@@ -69,5 +87,6 @@ button3.padding(5,5)
 button4.padding(5,5)
 button5.padding(5,5)
 button6.padding(5,5)
+button7.padding(5,5)
 
 app.display()
