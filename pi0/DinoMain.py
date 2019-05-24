533d5884550d26f3901115b9b0c214d9268cd9f9
fbourbour <fredbourbour@comcast.net>
2019-05-24 04:28:03 +0000

    Serial Port Access Improved

--- pi0/DinoMain.py
+++ pi0/DinoMain.py
@@ -2,6 +2,7 @@
 from time               import *  # Time library
 import os
 import sys
+import serial
 
 # Project interfaces
 from DinoConstants      import *  # Project constants
@@ -16,6 +17,11 @@ from DinoSpectrometer   import *  # Spectrometer interface
 
 
 THERMAL_CONTROL_PERIOD = 10 # Unit: 1/10 sec 
+MAXBUFFER = 200
+NUMDATAFIELDS = 21
+PORTNAME = '/dev/serial0'
+BAUDRATE = 115200
+TIMEOUT = 0.02
 
 
 DINO_STATE_INIT       = 0
@@ -24,6 +30,14 @@ DINO_STATE_EXPERIMENT = 2
 DINO_STATE_END_EXP    = 3
 DINO_STATE_FINISHED   = 4
 
+FLIGHT_DATA = {'flight_state': 0, 
+                'exptime': 0,
+                'altitude': 0,
+                'velocity': [0,0,0],
+                'acceleration': [0,0,0],
+                'attitude': [0,0,0],
+                'angular_velocity': [0,0,0],
+                'warnings': [0,0,0,0,0,0]}
 
 class DinoMain(object):
 
@@ -45,9 +59,13 @@ class DinoMain(object):
          DinoMain._dinoEnv     = DinoEnvirophat()
          DinoMain._dinoThermal = DinoThermalControl(HEATER_PIN, COOLER_PIN)
          DinoMain._dinoServo   = DinoServo(SERVO_PIN)
-         DinoMain._dinoSerial  = DinoSerial('/dev/ttyAMA0')
+         DinoMain._dinoSerial  = DinoSerial('/dev/serial0')
+         DinoMain._dinoSpectrometer = DinoSpectrometer()
 
-         # Initialize sensor data touple
+         #initialize the spectrometer
+         DinoMain._dinoSpectrometer.initialize()
+         
+         # Initialize sensor data tuple
          DinoMain._data        = [None,] * I_SIZE
 
          # Experiment state
@@ -66,15 +84,19 @@ class DinoMain(object):
    def _readAllData(self):
       """
       Read/parse serial data and all sensors from Envirophat.
-      Store a touple of sensor readings with "False" 
+      Store a tuple of sensor readings with "False" 
       for any fields that could not be read.
       """
-      tempSerial = self._dinoSerial.readData()
+      flight_state = FLIGHT_DATA['flight_state']#self._dinoSerial.readData()
+      flight_altitude = FLIGHT_DATA['altitude']
+      flight_acceleration = FLIGHT_DATA['acceleration']
       tempEnv    = self._dinoEnv.readData()
+      #print('flight_state =')
+      #print(flight_state)
 
-      self._data[I_FLIGHT_STATE]    = tempSerial[NR_FLIGHT_STATE]
-      self._data[I_ALTITUDE]        = tempSerial[NR_ALTITUDE]
-      self._data[I_ACCELERATION]    = tempSerial[NR_ACCELERATION]
+      self._data[I_FLIGHT_STATE]    = flight_state #tempSerial[NR_FLIGHT_STATE]
+      self._data[I_ALTITUDE]        = flight_altitude #tempSerial[NR_ALTITUDE]
+      self._data[I_ACCELERATION]    = flight_acceleration #tempSerial[NR_ACCELERATION]
       self._data[I_LIGHT_RED]       = tempEnv[ENV_HAT_LIGHT_RED]
       self._data[I_LIGHT_GREEN]     = tempEnv[ENV_HAT_LIGHT_GREEN]
       self._data[I_LIGHT_BLUE]      = tempEnv[ENV_HAT_LIGHT_BLUE]
@@ -85,7 +107,61 @@ class DinoMain(object):
       self._data[I_ACCEL_Y]         = tempEnv[ENV_HAT_ACCEL_Y]
       self._data[I_ACCEL_Z]         = tempEnv[ENV_HAT_ACCEL_Z]
       
+   def parse_serial_packet(self,incoming_data):
+    # Remove leading or trailing white space and separate the fields.
+    incoming_data = incoming_data.strip()
+    fields = incoming_data.split(b',')
 
+    # Ensure that the appropriate number of data fields are present.
+    if len(fields) != NUMDATAFIELDS:
+        return False
+    else:
+        index = 0
+        for field in fields:
+            if index == 0:
+                FLIGHT_DATA['flight_state'] = field 
+            elif index == 1:
+                FLIGHT_DATA['exptime'] = float(field)
+            elif index == 2:
+                FLIGHT_DATA['altitude'] = float(field)
+            elif index == 3:
+                FLIGHT_DATA['velocity'][0] = float(field)
+            elif index == 4:
+                FLIGHT_DATA['velocity'][1] = float(field)
+            elif index == 5:
+                FLIGHT_DATA['velocity'][2] = float(field)
+            elif index == 6:
+                FLIGHT_DATA['acceleration'][0] = float(field)
+            elif index == 7:
+                FLIGHT_DATA['acceleration'][1] = float(field)
+            elif index == 8:
+                FLIGHT_DATA['acceleration'][2] = float(field)
+            elif index == 9:
+                FLIGHT_DATA['attitude'][0] = float(field)
+            elif index == 10:
+                FLIGHT_DATA['attitude'][1] = float(field)
+            elif index == 11:
+                FLIGHT_DATA['attitude'][2] = float(field)
+            elif index == 12:
+                FLIGHT_DATA['angular_velocity'][0] = float(field)
+            elif index == 13:
+                FLIGHT_DATA['angular_velocity'][1] = float(field)
+            elif index == 14:
+                FLIGHT_DATA['angular_velocity'][2] = float(field)
+            elif index == 15:
+                FLIGHT_DATA['warnings'][0] = int(field)
+            elif index == 16:
+                FLIGHT_DATA['warnings'][1] = int(field)
+            elif index == 17:
+                FLIGHT_DATA['warnings'][2] = int(field)
+            elif index == 18:
+                FLIGHT_DATA['warnings'][3] = int(field)
+            elif index == 19:
+                FLIGHT_DATA['warnings'][4] = int(field)
+            elif index == 20:
+                FLIGHT_DATA['warnings'][5] = int(field)
+            index = index + 1
+        return True
    def _determineState(self):
       """
       Determine the state of the experiment.
@@ -95,10 +171,14 @@ class DinoMain(object):
       
       
       # Logic to determine the current experiment state
-      nrState = NR_STATE_LETTERS.index(self._data[I_FLIGHT_STATE])
+      strState = self._data[I_FLIGHT_STATE].decode('utf-8')
+      nrState = NR_STATE_LETTERS.index(strState)
+      #print('strState = ',strState)
+      
       
       if(nrState < NR_STATE_MECO):
          self._currState = DINO_STATE_INIT
+         
          
       elif(nrState < NR_STATE_UNDER_CHUTE):
          if(self._currState == DINO_STATE_INIT):
@@ -160,14 +240,23 @@ class DinoMain(object):
    
       return True
    def run(self):
-
-
+      ser = serial.Serial(port=PORTNAME, baudrate=BAUDRATE, timeout=TIMEOUT)
+      #self._dinoSerial.openSerialPort()
       while(self._endTest == False):
+         #print("Reading the USB serial port")
+         #Read all sensor data and serial data
+         data_in = ser.read(MAXBUFFER)
+         
+         if (len(data_in) == 0):
+            continue
 
-         # Read all sensor data and serial data
+        # Check that packet was well formatted.
+         if not self.parse_serial_packet(data_in):
+            continue
+        
+         print(data_in)
          self._readAllData()
          DinoLog.logData(self._data)
-
          # Determine experiment state
          self._determineState()
 
@@ -181,6 +270,7 @@ class DinoMain(object):
             self._dinoCamera.startRecording(duration=CAMERA_REC_DURATION)
             self._dinoServo.startServo(SERVO_AGITATION_INTERVAL)
             #TODO Turn on spectrometer captures
+            self._dinoSpectrometer.captureSpectrum()
             
          elif(self._currState == DINO_STATE_EXPERIMENT):
             pass
