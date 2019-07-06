9da152d65cfd2476c9507c4c2a86698c6f2d656e
Dario Schor <dschor5@gmail.com>
2019-07-06 10:58:45 -0500

    Catch failures that could cause the log thread to stay open.

--- pi0/main.py
+++ pi0/main.py
@@ -2,6 +2,9 @@ from DinoMain import *
 
 if(__name__ == "__main__"):
    print("Start Dino Experiment.")
-   DinoMain().run()
+   try:
+      DinoMain().run()
+   except:
+      DinoLog.stop()
    print("End Dino Experiment.")
 
